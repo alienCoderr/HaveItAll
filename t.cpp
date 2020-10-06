a202116a6cc68e4e94bf0ea5b4761772996717ea
bugger-debugger <akshatjindal215@gmail.com>
2020-10-07 02:08:26 +0530

    Update t.cpp

--- t.cpp
+++ t.cpp
@@ -1,61 +1,30 @@
 #include <bits/stdc++.h>
 #define lli long long int
 #define ulli unsigned long long int
-#define ld long double
-#define vi vector<lli>
-#define precision(x,y)         fixed<<setprecision(y)<<x
 using namespace std;
-int ans = 0;
-int diameter(vector<vector<int>>& g, int src, int par){
-
-    int a = -1, b = -1;
-    for(auto x: g[src]){
-        if(x == par)
-            continue;
-        int len = diameter(g, x, src);
-        if(len >= b){
-            a = b;
-            b = len;
-        }else if(len > a && len <= b)
-            a = len;
+ulli fun(ulli a, ulli b, ulli mod){
+ 
+    ulli ans=1;
+    while(b>0){
+        if(b&1)
+            ans=((ans%mod)*(a%mod))%mod;
+        b/=2;
+        a = ((a%mod)*(a%mod))%mod;
     }
-
-    if(a!=-1 && b!=-1){
-        ans=max(ans, a+b+2);
-        return max(a+1, b+1);
-    }
-    //a=-1, b!=-1
-    if(b!=-1){
-        ans=max(ans,b+1);
-        return b+1;
-    }
-    //a=-1, b=-1
-    return 0;
+    return ans%mod;
 }
 void solve(){
-    int n;
-    cin>>n;
-    vector<vector<int>> g(n);
-    int t=n-1;
-    while(t--){
-        int a,b;
-        cin>>a>>b;
-        g[a-1].push_back(b-1);
-        g[b-1].push_back(a-1);
-    }
-    ans = 0;
-    diameter(g, 0, -1);
-    cout<<ans<<endl;
-
+    ulli a, b, c;
+    cin>>a>>b>>c;
+    cout<<fun(a,fun(b,c,1000000006),1000000007)<<endl;
 }
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
     int t = 1;
-    // cin>>t;
+    cin>>t;
     while(t--){
         solve();
     }
 }
-
